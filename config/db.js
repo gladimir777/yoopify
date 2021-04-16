30719f37bbe9068c79bbc84ee8e87bcf3126f3ad
gladimir777 <gladimir777@github.com>
2021-04-15 23:00:48 -0400

    adding doteenv dependencies

--- config/db.js
+++ config/db.js
@@ -1,10 +1,10 @@
 const mongoose = require("mongoose");
-
+require("dotenv").config();
 const dbConnection = () => {
-  mongoose.connect(
-    "mongodb+srv://devconector:devconector123@devconector-vuu2z.mongodb.net/test?retryWrites=true&w=majority",
-    { useNewUrlParser: true, useUnifiedTopology: true }
-  );
+  mongoose.connect(process.env.DB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
 
   const db = mongoose.connection;
   db.on("error", (error) => console.log("connection fail", error));
